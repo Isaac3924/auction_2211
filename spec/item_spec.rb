ea288daa17ed602443685cd6c474263d9d8bd3d9
Isaac Alter <i_alter@yahoo.com>
2023-01-08 15:33:35 -0700

    Add feat: more framework and first test in item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,5 +2,13 @@ require 'rspec'
 require '.lib/item'
 
 RSpec.describe Item do
-    
+    describe "Iteration 1" do
+        let(:item1){ Item.new('Chalkware Piggy Bank') }
+        let(:item2){ Item.new('Bamboo Picture Frame') }
+
+        it "#initializes" do
+            expect(item1).to be_a(Item)
+            expect(item2).to be_a(Item)
+        end
+    end
 end
