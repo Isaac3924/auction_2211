1fcbebafa07d8aa6afefc35f70347028af76a31f
Isaac Alter <i_alter@yahoo.com>
2023-01-08 15:52:44 -0700

    Add feat: all tests for auction_spec for iteration 1

--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -1,2 +1,34 @@
 require 'rspec'
-require '.lib/auctions'
+require './lib/auctions'
+require './lib/item'
+
+RSpec.describe Auction do
+    describe "Iteration 1" do
+        let(:auction){ Auction.new }
+
+        let(:item1){ Item.new('Chalkware Piggy Bank') }
+        let(:item2){ Item.new('Bamboo Picture Frame') }
+
+        xit "#initializes" do
+            expect(auction).to be_a(Auction)
+        end
+
+        xit "can read attributes" do
+            expect(auction.items).to eq([])
+        end
+
+        xit "#add_item" do
+            auction.add_item(item1)
+            auction.add_item(item2)
+
+            expect(auction.items).to eq([item1, item2])
+        end
+
+        xit "#item_names" do
+            auction.add_item(item1)
+            auction.add_item(item2)
+
+            expect(auction.item_names).to eq(['Chalkware Piggy Bank', 'Bamboo Picture Frame'])
+        end
+    end
+end
