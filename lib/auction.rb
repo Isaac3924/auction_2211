03852000fa27a4bcc9ac530d169b2453db5c64f8
Isaac Alter <i_alter@yahoo.com>
2023-01-10 11:05:03 -0700

    THis is the review for iteration 3 and 4 with Abdul. Unfinished. Need to add in logic for first part of iteration 4. Refer to the video in Slack within codehelp in the thread, archive of videos is pinned at top as well

--- lib/auction.rb
+++ lib/auction.rb
@@ -1,3 +1,5 @@
+require "date"
+
 class Auction
     attr_reader :items
 
@@ -40,39 +42,97 @@ class Auction
     end
 
     def bidders
-        all_items_bids = items.find_all do |item|
-            item.bids != {}
+        attendees = []
+
+        @items.each do |item|
+          item.bids.keys.each do |attendee|
+            attendees << attendee
+          end
         end
 
-        all_bids = []
+        attendees.uniq #.uniq gets rid of duplicates
 
-        all_items_bids.each do |item|
-            item.bids.keys.each do |attendee|
-                if all_bids.include?(attendee) == false
-                    all_bids << attendee
+        # all_items_bids = items.find_all do |item|
+        #     item.bids != {}
+        # end
+
+        # all_bids = []
+
+        # all_items_bids.each do |item|
+        #     item.bids.keys.each do |attendee|
+        #         if all_bids.include?(attendee) == false
+        #             all_bids << attendee
+        #         end
+        #     end
+        # end
+
+        # all_bids
+    end
+
+    def bidder_info
+      bidder_info_hash = {}
+
+        bidders.each do |attendee|
+          bidder_info_hash[attendee] = {
+            budget: attendee.budget,
+            items: items_by_attendee(attendee)
+          }
+        end
+      
+      bidder_info_hash
+        # bidder_info = Hash.new { |attendee, info| attendee[info] = { } }
+
+        # all_items_bid_on = items.find_all do |item|
+        #     item.bids != {}
+        # end
+
+        # all_items_bid_on.find_all do |item|
+        #     item.bids.keys.each do |attendee|
+        #         if 
+        #     end
+        # end
+
+        # bidders.each do |bidder|
+        #     require 'pry'; binding.pry
+        #     bidder_info[bidder] = { budget: bidder.budget, items: [] }
+        # end
+    end
+
+    def items_by_attendee(attendee)
+      @items.select do |item|
+        item.bids.keys.include? attendee
+      end  
+    end
+
+    def generate_date
+      Date.today
+    end
+
+    def date
+        generate_date.strftime("%d/%m/%Y")
+    end
+
+    def close_auction
+      closing_hash = {}
+
+      items_by_highest_bid = @items.sort_by do |item|
+        item.current_high_bid
+      end.reverse
+
+      items_by_highest_bid.each do |item|
+        if item.bids.empty?
+            closing_hash[item] = "Not Sold"
+        else
+            bidders_by_bid_amt_desc = item.bids.sort_by do |_, bid_amt|
+                if attendee.budget >= bid_amt
+                    attendee.budget -= bid_amt
+                    closing_hash[item] = attendee
+                    break
                 end
             end
         end
 
-        all_bids
-    end
 
-    def bidder_info
-        bidder_info = Hash.new { |attendee, info| attendee[info] = { } }
-
-        all_items_bid_on = items.find_all do |item|
-            item.bids != {}
-        end
-
-        all_items_bid_on.find_all do |item|
-            item.bids.keys.each do |attendee|
-                if 
-            end
-        end
-
-        bidders.each do |bidder|
-            require 'pry'; binding.pry
-            bidder_info[bidder] = { budget: bidder.budget, items: [] }
-        end
+      end
     end
 end
--- spec/auction_spec.rb
+++ spec/auction_spec.rb
@@ -106,20 +106,63 @@ RSpec.describe Auction do
         it "#bidder_info" do
             expected = { 
                           attendee1 => {
-                                :budget => 50,
+                                :budget => "$50",
                                 :items => [item1]
                             },
                             attendee2 => {
-                                :budget => 75,
+                                :budget => "$75",
                                 :items => [item1, item3]
                             },
                             attendee3 => {
-                                :budget => 100,
+                                :budget => "$100",
                                 :items => [item4]
                             } 
             }
 
             expect(auction.bidder_info).to eq(expected)
+        end
+
+        it "#date" do
+          allow(auction).to receive(:generate_date).and_return(Date.new(1999, 1, 8))
+
+          expect(auction.date).to eq("08/01/1999")
+        end
+    end
+
+    describe "Iteration 4" do
+        let(:item1){ Item.new('Chalkware Piggy Bank') }
+        let(:item2){ Item.new('Bamboo Picture Frame') }
+        let(:item3){ Item.new('Homemade Chocolate Chip Cookies') }
+        let(:item4){ Item.new('2 Days Dogsitting') }
+        let(:item5){ Item.new('Forever Stamps') }
+
+        let(:attendee1){ Attendee.new({name: 'Megan', budget: '$50'}) }
+        let(:attendee2){ Attendee.new({name: 'Bob', budget: '$75'}) }
+        let(:attendee3){ Attendee.new({name: 'Mike', budget: '$100'}) }
+
+        let(:auction){ Auction.new }
+
+        before do 
+            auction.add_item(item1)
+            auction.add_item(item2)
+            auction.add_item(item3)
+            auction.add_item(item4)
+            auction.add_item(item5)
+
+            item1.add_bid(attendee1, 22)
+            item1.add_bid(attendee2, 20)
+            item3.add_bid(attendee2, 30)
+            item4.add_bid(attendee3, 50)
+        end
+
+        it "#close_auction" do
+          expect(auction.close_auction).to match({
+            item1 => attendee1,
+            item2 => "Not Sold",
+            item3 => attendee2,
+            item4 => attendee3,
+            item5 => "Not Sold"
+          })
         end
     end
 end
